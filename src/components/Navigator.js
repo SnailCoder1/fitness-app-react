ec375c8f1ffd41089ab708eefea1933ec55a7673
Boa <boa.matule@gmail.com>
2020-01-31 17:51:51 +0100

    Adds a container to handle input that the keyboard needs to deal with. Uses the KeyboardAvoidingView and adds some active event style padding; Makes the use of the ScrollView for a easy scrollable; Sets LoginScreen as the initial screen in the StackNavigator; Adds the Logo wrapping it with container and styles it. WIP:

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,15 +1,27 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import IntroScreen from '../screens/IntroScreen';
-// import LoginScreen from '../screens/LoginScreen';
+import LoginScreen from '../screens/LoginScreen';
 // import EmailInputScreen from '../screens/EmailInputScreen';
 // import PasswordInputScreen from '../screens/PasswordInputScreen';
 
-const StackNavigator = createStackNavigator({
-  IntroScreen: IntroScreen,
-  // LoginScreen: LoginScreen,
-  // EmailInputScreen: EmailInputScreen,
-  // PasswordInputScreen: PasswordInputScreen,
-});
+const StackNavigator = createStackNavigator(
+  {
+    onBoardScreen: {
+      screen: IntroScreen,
+    },
+    // EmailInputScreen: EmailInputScreen,
+    LoginScreen: LoginScreen,
+    // TouchAuthentication: TouchAuthentication,
+    // PasswordInputScreen: PasswordInputScreen,
+    // SelectProfileScreen: SelectProfileScreen,
+    // SetGoalScreen: SetGoalScreen,
+    // CustomizeInterest: CustomizeInterest,
+    // SelectGender: SelectGender,
+  },
+  {
+    initialRouteName: 'LoginScreen',
+  }
+);
 
 export default createAppContainer(StackNavigator);
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, ActivityIndicator, TouchableOpacity, KeyboardAvoidingView, scrollView,} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+} from 'react-native';
 import {Text, Icon, Input, Button, SocialIcon} from 'react-native-elements';
 
-export default class LoginScreen extends Component  {
+export default class LoginScreen extends Component {
+  static navigationOptions = {
+    headerShown: false,
+  };
+
   render() {
     return (
-      
-    )
+      <KeyboardAvoidingView behavior={'padding'} enabled>
+        <View style={styles.headerContainer}>
+          <Icon name="md-fitness" size={80} type="ionicon" color={'#7265E2'} />
+        </View>
+        <ScrollView
+          style={StyleSheet.container}
+          keyboardShouldPersistTaps="handled"
+        />
+      </KeyboardAvoidingView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4F6FA',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerContainer: {
+    top: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+  },
+});

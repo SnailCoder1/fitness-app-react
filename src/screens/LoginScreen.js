cd9241cfbb70a19a993dc58d8a0101150b1bc600
Boa <boa.matule@gmail.com>
2020-01-31 17:08:26 +0100

    Imports all the necessary components for the login screen. WIP

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, ActivityIndicator, TouchableOpacity, KeyboardAvoidingView, scrollView,} from 'react-native';
+import {Text, Icon, Input, Button, SocialIcon} from 'react-native-elements';
+
+export default class LoginScreen extends Component  {
+  render() {
+    return (
+      
+    )
+  }
+}

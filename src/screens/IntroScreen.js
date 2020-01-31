2a1311561302333d8975409d82209ce6da22d618
Boa <boa.matule@gmail.com>
2020-01-31 16:02:16 +0100

    Installs react-native-elements; Imports the necessary components to the introScreen; creates the main global wrapper and header section and styles it; Adds react-native-vector-icons and register the font in the info.plist. WIP

--- src/screens/IntroScreen.js
+++ src/screens/IntroScreen.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import {Text, Icon, Image, Button} from 'react-native-elements';
 
-export class IntroScreen extends Component {
+export default class IntroScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> IntroScreen </Text>
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Icon name="md-fitness" size={80} type="ionicon" />
+          <Text h4>Welcome to Fitness Master</Text>
+          <Text h5 style={{textAlign: 'center'}}>
+            The best App for track Fitness workout and Meal planner
+          </Text>
+        </View>
       </View>
     );
   }
 }
-export default IntroScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F4F6FA',
+    height: '100%',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  headerContainer: {
+    top: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 40,
+  },
+});

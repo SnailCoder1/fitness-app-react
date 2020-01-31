debae9dd4aa05eea8ce02c1887ea702fda59e01a
Boa <boa.matule@gmail.com>
2020-01-31 18:10:24 +0100

    Adds Email and Password inputs and style the inputs. WIP

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -20,10 +20,48 @@ export default class LoginScreen extends Component {
         <View style={styles.headerContainer}>
           <Icon name="md-fitness" size={80} type="ionicon" color={'#7265E2'} />
         </View>
-        <ScrollView
-          style={StyleSheet.container}
-          keyboardShouldPersistTaps="handled"
-        />
+        <View style={styles.wrapper}>
+          <Input
+            leftIcon={
+              <Icon
+                name="email-outline"
+                type="material-community"
+                color="rgba(110, 120, 170, 1)"
+                size={25}
+              />
+            }
+            placeholder="Email"
+            inputContainerStyle={styles.input}
+            placeholderTextColor="grey"
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="email-address"
+            returnKeyType="next"
+          />
+          <Input
+            leftIcon={
+              <Icon
+                name="lock"
+                type="simple-line-icon"
+                color="rgba(110, 120, 170, 1)"
+                size={25}
+              />
+            }
+            inputContainerStyle={styles.input}
+            placeholderTextColor="grey"
+            placeholder="Password"
+            autoCapitalize="none"
+            secureTextEntry={true}
+            autoCorrect={false}
+            keyboardType="default"
+            returnKeyType="next"
+          />
+
+          <ScrollView
+            style={StyleSheet.container}
+            keyboardShouldPersistTaps="handled"
+          />
+        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -41,5 +79,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 40,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'white',
+    borderLeftWidth: 0,
+    height: 50,
+    backgroundColor: 'white',
+    marginBottom: 20,
   },
 });
